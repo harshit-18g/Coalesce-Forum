f4f0ddb2859c3e8fb4da5917e0831b4e40b3271c
harshit-18g <85693217+harshit-18g@users.noreply.github.com>
2022-01-14 20:40:53 +0530

    Add files via upload

---
+++ src/shared/items.js
@@ -0,0 +1,17 @@
+export const ITEMS =
+    [
+        {
+        id: 0,
+        name:'Department/Club/Candidate - 1',
+        image: '/assets/images/1.png',
+        featured: true,
+        description:'About Department/Club/Candidate'                    
+        },
+        {
+        id: 1,
+        name:'Department/Club/Candidate - 2',
+        image: '/assets/images/2.png',
+        featured: false,
+        description:'About Department/Club/Candidate'
+        }
+    ];
