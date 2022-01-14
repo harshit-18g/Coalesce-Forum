792a904e93afef30fb4f8adb1615c437dc5ea5d6
harshit-18g <85693217+harshit-18g@users.noreply.github.com>
2022-01-14 20:40:32 +0530

    Create comments.js

---
+++ src/shared/comments.js
@@ -0,0 +1,83 @@
+export const COMMENTS = 
+[
+    {
+        id: 0,
+        dishId: 0,
+        rating: 5,
+        comment: "Comment 1",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 1,
+        dishId: 0,
+        rating: 4,
+        comment: "Comment 2",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 2,
+        dishId: 0,
+        rating: 3,
+        comment: "Comment 3",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 3,
+        dishId: 0,
+        rating: 4,
+        comment: "Comment 4",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 4,
+        dishId: 0,
+        rating: 2,
+        comment: "Comment 5",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 5,
+        dishId: 1,
+        rating: 5,
+        comment: "Comment 1",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 6,
+        dishId: 1,
+        rating: 4,
+        comment: "Comment 2",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 7,
+        dishId: 1,
+        rating: 3,
+        comment: "Comment 3",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 8,
+        dishId: 1,
+        rating: 4,
+        comment: "Comment 4",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 9,
+        dishId: 1,
+        rating: 2,
+        comment: "Comment 5",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    }
+];
